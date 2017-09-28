c470e9ed0970068783701bd88ff34655cb070f18
nyg4628 <nyg4628@gmail.com>
2017-09-28 17:54:13 +0900

    modified ...;

--- public/chat.js
+++ public/chat.js
@@ -3,7 +3,7 @@ var socket = io.connect('http://localhost:4000');
 
 //  Query DOM
 var message     = document.getElementById('message'),
-    handle      = document.getElementById('handle'),
+    nickname    = document.getElementById('nickname'),
     btn         = document.getElementById('send'),
     output      = document.getElementById('output'),
     feedback    = document.getElementById('feedback');
@@ -11,21 +11,27 @@ var message     = document.getElementById('message'),
 // Emit events
 btn.addEventListener('click', function () {
     socket.emit('chat', {
-        message: message.value,
-        handle: handle.value
+        nickname: nickname.value,
+        message: message.value
     });
 });
 
-message.addEventListener('keypress', function () {
-    socket.emit('typing', handle.value);
+message.addEventListener('keyup', function () {
+    socket.emit('typing', {
+        nickname: nickname.value,
+        message: message.value
+    });
 });
 
 // Listen for events
 socket.on('chat', function (data) {
     feedback.innerHTML = '';
-    output.innerHTML += '<p><strong>' + data.handle + ' : </strong>' + data.message + '</p>';
+    output.innerHTML += '<p><strong>' + data.nickname + ' : </strong>' + data.message + '</p>';
 });
 
 socket.on('typing', function (data) {
-    feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
+    if (data.message)
+        feedback.innerHTML = '<p><em>' + data.nickname + ' is typing a message...</em></p>';
+    else 
+        feedback.innerHTML = '';
 });
